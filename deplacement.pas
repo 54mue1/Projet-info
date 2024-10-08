cbcd54a9b4ab1ab6ed1a95e844ce5c461943b488
Preuxchevalier <titouan.glemarec@insa-rouen.fr>
2024-10-08 20:39:00 +0200

    Update deplacement.pas

--- deplacement.pas
+++ deplacement.pas
@@ -4,17 +4,25 @@ Interface
 
 uses crt, types_demineur;
 
-procedure deplacer(var grille: tab);
+procedure deplacer(var curs : curseur);
 
 
 Implementation
 
-procedure deplacer(var grille : tab);
+
+procedure deplacer(var curs:curseur);
+var touche:char;
 begin
-	case READKEY of 
-		#72 : then  // aller en haut en ayant vérifier qu'on ait pas y=0
-		#80 : then  // aller en bas en ayant vérifier qu'on ait pas y=MAX
-		#77 : then  // aller à droite en ayant vérifier qu'on ait pas x=MAX
-		#75 : then  // aller à gauche en ayant vérifier qu'on ait pas x=0
-	end;
+    touche := ReadKey;
+    case touche of
+        #72: if curs.y > 1 then curs.y := curs.y - 1;  // Haut (code ASCII de la touche flèche haute)
+        #80: if curs.y < MAX then curs.y := curs.y + 1;  // Bas (code ASCII de la touche flèche basse)
+        #75: if curs.x > 1 then curs.x := curs.x - 1;  // Gauche (code ASCII de la touche flèche gauche)
+        #77: if curs.x < MAX then curs.x := curs.x + 1;  // Droite (code ASCII de la touche flèche droite)
+    end;
+end;
+
+
+
+
 end.
