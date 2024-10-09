97d5b551579fef6cf29b664decc7f270fc2cafa6
pauloow14 <paul.gauclin@laposte.net>
2024-10-09 11:09:54 +0200

    Update checkBombes.pas

--- checkBombes.pas
+++ checkBombes.pas
@@ -14,10 +14,25 @@ procedure checkCaseAutour(i,j:Integer; var grille: tab);
 var m,n,nbBombes:Integer;
 begin
 nbBombes:=0;
-for m:=-1 to 1 do
-	for n:=-1 to 1 do
-		if grille[i+m,j+n].mine then
-			nbBombes:=nbBombes+1;
+if i=1 then
+	begin
+		if j=1 then
+		if j=MAX then
+		else
+	end
+else if i=MAX then
+else if j=1 then
+	begin
+		if j=1 then
+		if j=MAX then
+		else
+	end
+else if j=MAX then
+else
+	for m:=-1 to 1 do
+		for n:=-1 to 1 do
+			if grille[i+m,j+n].mine then
+				nbBombes:=nbBombes+1;
 grille[i,j].nbMinesAutour:=nbBombes;
 end;
 
