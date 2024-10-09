3d5f4faec7635a1797d53d9e10064430c2b04930
pauloow14 <paul.gauclin@laposte.net>
2024-10-09 16:30:03 +0200

    Update checkBombes.pas

--- checkBombes.pas
+++ checkBombes.pas
@@ -10,34 +10,116 @@ procedure remplirNbBombes(var grille: tab);
 
 Implementation	
 
-procedure checkCaseAutour(i,j:Integer; var grille: tab);
+procedure checkCaseAutour(i,j:Integer; var grille: tab); // compte le nombre de bombes autour d'une case
 var m,n,nbBombes:Integer;
 begin
 nbBombes:=0;
-if i=1 then
+if j=1 then //comptage bombes autour des cases de la ligne du haut
 	begin
-		if j=1 then
-		if j=MAX then
-		else
+		if i=1 then // case haut gauche
+			begin
+				if grille[i,j+1].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i+1,j].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i+1,j+1].mine then
+					nbBombes:=nbBombes+1;
+			end;
+		if i=MAX then // case haut droit
+			begin
+				if grille[i-1,j].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i-1,j+1].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i,j+1].mine then
+					nbBombes:=nbBombes+1;
+			end			
+		else // reste des cases de la ligne
+			begin
+				if grille[i-1,j].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i+1,j].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i-1,j+1].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i,j+1].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i+1,j+1].mine then
+					nbBombes:=nbBombes+1;
+			end
 	end
-else if i=MAX then
-else if j=1 then
+else if i=1 then //comptage bombes autour des cases de la colonne de gauche
 	begin
-		if j=1 then
-		if j=MAX then
-		else
-	end
-else if j=MAX then
-else
-	for m:=-1 to 1 do
-		for n:=-1 to 1 do
-			if grille[i+m,j+n].mine then
+			if grille[i,j-1].mine then
 				nbBombes:=nbBombes+1;
+			if grille[i,j+1].mine then
+				nbBombes:=nbBombes+1;
+			if grille[i+1,j-1].mine then
+				nbBombes:=nbBombes+1;
+			if grille[i+1,j].mine then
+				nbBombes:=nbBombes+1;
+			if grille[i+1,j+1].mine then
+				nbBombes:=nbBombes+1;
+	end
+else if j=MAX then //comptage bombes autour des cases de la ligne du bas
+	begin
+		if i=1 then // case bas gauche
+			begin
+				if grille[i,j-1].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i+1,j-1].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i+1,j].mine then
+					nbBombes:=nbBombes+1;
+			end;
+		if i=MAX then // case bas droit
+			begin
+				if grille[i,j-1].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i-1,j-1].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i-1,j].mine then
+					nbBombes:=nbBombes+1;
+			end
+		else // reste des cases de la ligne
+			begin
+				if grille[i-1,j].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i-1,j-1].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i,j-1].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i+1,j-1].mine then
+					nbBombes:=nbBombes+1;
+				if grille[i+1,j].mine then
+					nbBombes:=nbBombes+1;
+			end
+	end
+else if i=MAX then //comptage bombes autour des cases de la colonne de droite
+	begin
+			if grille[i,j-1].mine then
+				nbBombes:=nbBombes+1;
+			if grille[i,j+1].mine then
+				nbBombes:=nbBombes+1;
+			if grille[i-1,j-1].mine then
+				nbBombes:=nbBombes+1;
+			if grille[i-1,j].mine then
+				nbBombes:=nbBombes+1;
+			if grille[i-1,j+1].mine then
+				nbBombes:=nbBombes+1;
+	end
+else 
+	begin //comptage bombes dans tous le reste de la grille 
+		for m:=-1 to 1 do
+			for n:=-1 to 1 do
+				if grille[i+m,j+n].mine then
+					nbBombes:=nbBombes+1;
+	end;
 grille[i,j].nbMinesAutour:=nbBombes;
 end;
 
 
-procedure remplirNbBombes(var grille: tab);
+procedure remplirNbBombes(var grille: tab); // compte le nombre de bombes autour de chaque case de la grille
 var i,j:Integer;
 begin
 for i:=1 to MAX do 
