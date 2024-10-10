bd7733456d00c9052fac80e9be379bdb33e4aa4b
Preuxchevalier <titouan.glemarec@insa-rouen.fr>
2024-10-10 15:19:15 +0200

    Update deplacement.pas
    fusion de deplacement, drapeau et devoiler

--- deplacement.pas
+++ deplacement.pas
@@ -4,16 +4,14 @@ Interface
 
 uses crt, types_demineur;
 
-procedure deplacer(var curs : curseur);
+procedure deplacer(var curs : curseur; var grille :tab);
 
-procedure devoiler(curs: curseur; var grille : tab);
 
-procedure drapeau(curs : curseur; var grille : tab);
 
 Implementation
 
 
-procedure deplacer(var curs:curseur);
+procedure deplacer(var curs:curseur;var grille:tab);
 var touche:char;
 begin
     touche := READKEY;
@@ -22,21 +20,10 @@ begin
         BAS: if curs.y < MAX then curs.y := curs.y + 1;  // déplacement bas
         GAUCHE: if curs.x > 1 then curs.x := curs.x - 1;  // déplacement gauche
         DROITE: if curs.x < MAX then curs.x := curs.x + 1;  // déplacement droite
+        ENTREE: if not(grille[curs.y][curs.x].estAffiche) then grille[curs.y][curs.x].estAffiche:=True; // rends la case sur laquelle est le curseur dévoilable
+        INSER :  if not(grille[curs.y][curs.x].drapeau)   then grille[curs.y][curs.x].drapeau:=True;  // pose un drapeau ....
     end;
 end;
 
-procedure devoiler(curs:curseur ;var grille:tab);
-var touche :char;
-begin
-touche:=READKEY;
-	If touche= ENTREE then grille[curs.x][curs.y].estAffiche:=True; // rends la case sur laquelle est le curseur dévoilable
-end;
-
-procedure drapeau(curs : curseur; var grille: tab);
-var touche : char;
-begin
-touche:=READKEY;
-	If touche= INSER then grille[curs.x][curs.y].drapeau:=True; // pose un drapeau sur la case pointée par le curseur
-end;
 
 end.
