a835fc653a0f4a40f52c85ca69c8266b698b818b
54mue1 <107001860+54mue1@users.noreply.github.com>
2024-10-10 12:28:08 +0200

    Update afficheGrille.pas

--- afficheGrille.pas
+++ afficheGrille.pas
@@ -2,15 +2,15 @@ unit afficheGrille;
 
 Interface
 
-uses types_demineur;
+uses types_demineur,crt;
 
 var i,j,k : Integer;
 
-procedure affiche(grille:tab);
+procedure affiche(grille:tab;curs:curseur);
 
 implementation
 
-procedure affiche(grille:tab);
+procedure affiche(grille:tab;curs:curseur);
 begin
 	for i := 1 to MAX do 
 		begin
@@ -18,16 +18,32 @@ begin
 				begin 
 					if grille[i][j].estAffiche then
 						begin	
+							TextColor(6);
 							write(grille[i][j].nbMinesAutour);
+							write(' ');
+						end
+					else if grille[i][j].drapeau then
+						begin	
+							TextColor(12);
+							write('♥');
 							write(' ');
 						end
 					else
 						begin
+							TextColor(2);
 							write('□');
 							write(' ');
 						end;
 				end;
 			writeln();
 		end;
+	TextColor(10);
+	ClrScr;
+	GotoXY((curs.x)*2,(curs.y)*2);
+	if grille[i][j].estAffiche then
+		write(grille[i][j].nbMinesAutour)
+	else
+		write('□');
 end;
 end.
+
