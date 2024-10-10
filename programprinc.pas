be0808b984e1d4c7e8f3b749e3fb133da7ade027
Preuxchevalier <titouan.glemarec@insa-rouen.fr>
2024-10-10 15:16:54 +0200

    Update programprinc.pas
    ajout du repeat until et de writeln,
     reste à gérer la perte de la partie

--- programprinc.pas
+++ programprinc.pas
@@ -1,10 +1,22 @@
 program demineur;
 
-uses types_demineur,checkBombes, creationGrille, afficheGrille, deplacement;
+
+uses types_demineur,checkBombes, creationGrille, afficheGrille, deplacement,crt;
 
 var grille:tab;
+curs : curseur;
 begin
+k:=0;
+curs.x:=1;
+curs.y:=1;
 creeGrille(grille);
 remplirNbBombes(grille);
-affiche(grille);
+repeat
+	affiche(grille,curs);
+	deplacer(curs,grille);
+	writeln();
+	clrscr;
+	writeln(curs.x,curs.y);
+	writeln(grille[curs.x][curs.y].estAffiche);
+until k=10 ;
 end.
