60b2da93eb97e833c45492d4dfed72e8ae262cc3
Preuxchevalier <titouan.glemarec@insa-rouen.fr>
2024-11-04 12:33:22 +0100

    Update programprinc.pas

--- programprinc.pas
+++ programprinc.pas
@@ -1,22 +1,20 @@
 program demineur;
 
-
-uses types_demineur,checkBombes, creationGrille, afficheGrille, deplacement,crt;
+uses types_demineur,checkBombes, creationGrille, afficheGrille, deplacement,crt,zeros;
 
 var grille:tab;
 curs : curseur;
 begin
-k:=0;
+j:=0;
 curs.x:=1;
 curs.y:=1;
 creeGrille(grille);
 remplirNbBombes(grille);
 repeat
-	affiche(grille,curs);
-	deplacer(curs,grille);
-	writeln();
-	clrscr;
-	writeln(curs.x,curs.y);
-	writeln(grille[curs.x][curs.y].estAffiche);
-until k=10 ;
+clrscr;
+//zeros(grille);
+
+affiche(grille,curs);
+deplacer(curs,grille);
+until j=10 ;
 end.
