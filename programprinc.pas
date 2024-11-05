14ad976d8deba6ed69a98ebb6edca618fad46ce5
pauloow14 <paul.gauclin@laposte.net>
2024-11-05 12:51:32 +0100

    Update programprinc.pas

--- programprinc.pas
+++ programprinc.pas
@@ -2,20 +2,22 @@ program demineur;
 
 uses types_demineur,checkBombes, creationGrille, afficheGrille, deplacement,crt,zero;
 
-var grille:tab;
+var grille1, grille2:tab;
 curs : curseur;
 begin
 j:=0;
 curs.x:=1;
 curs.y:=1;
-creeGrille(grille);
-remplirNbBombes(grille);
+creeGrille(grille1);
+remplirNbBombes(grille1);
+creeGrille(grille2);
+remplirNbBombes(grille2);
 repeat
 clrscr;
-zeros(grille);
-
-affiche1(grille,curs);
-affiche2(grille,curs);
+zeros(grille1);
+zeros(grille2);
+affiche1(grille1,curs);
+affiche2(grille2,curs);
 deplacer(curs,grille);
 
 
