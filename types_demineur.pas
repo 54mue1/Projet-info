a25fcd1ce46a9ec0c956c8c65effe9b1839e4e92
Preuxchevalier <titouan.glemarec@insa-rouen.fr>
2024-11-05 18:36:42 +0100

    Update types_demineur.pas

--- types_demineur.pas
+++ types_demineur.pas
@@ -3,14 +3,23 @@ unit types_demineur;
 Interface
 
 
-Const  MAX=8;
+Const 
+MAX=8; // taille des grilles (ici 8x8)
+// touches du clavier :
 HAUT=#72;
 BAS=#80;
 GAUCHE=#75;
 DROITE=#77;
 ENTREE=#13;
 INSER=#82;
-NB_BOMBES=11;
+Z=#44;
+Q=#16;
+S=#31;
+D=#32;
+A=#30;
+E=#18;
+NB_BOMBES=11; //nombre de bombes dans une grille
+//emplacement dans le terminal :
 XGRILLE1=20;
 YGRILLE=10;
 XGRILLE2=40;
