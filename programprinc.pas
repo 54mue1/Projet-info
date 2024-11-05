5053bbd4627a7554b24cc8c678793dcd5909e40e
Preuxchevalier <titouan.glemarec@insa-rouen.fr>
2024-11-05 13:10:56 +0100

    Update programprinc.pas

--- programprinc.pas
+++ programprinc.pas
@@ -25,3 +25,35 @@ clrscr;
 write('PERDU LOOSER'); ;
 end.
 
+//plus tard on aura : 
+
+
+program demineur;
+
+uses types_demineur,checkBombes, creationGrille, afficheGrille, deplacement,crt,zero,finPartie;
+
+var grille1, grille2:tab;
+curs1,curs2 : curseur;
+begin
+curs1.x:=1;
+curs1.y:=1;
+curs2.x:=1;
+curs2.y:=1;
+creeGrille(grille1);
+remplirNbBombes(grille1);
+creeGrille(grille2);
+remplirNbBombes(grille2);
+repeat
+clrscr;
+zeros(grille1);
+zeros(grille2);
+affiche(grille1,curs1,XGRILLE1,YGRILLE);
+affiche(grille2,curs2,XGRILLE2,YGRILLE);
+deplacer1(curs1,grille1);
+deplacer2(curs2,grille2);
+
+
+until finDePartie(grille1) or finDePartie(grille2);
+clrscr;
+write('PERDU LOOSER'); ;
+end.
