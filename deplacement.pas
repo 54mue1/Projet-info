36d54cca18ba4fa1ae85aa72141dac8b987c1c67
Preuxchevalier <titouan.glemarec@insa-rouen.fr>
2024-11-05 18:42:07 +0100

    Update deplacement.pas

--- deplacement.pas
+++ deplacement.pas
@@ -4,14 +4,15 @@ Interface
 
 uses crt, types_demineur;
 
-procedure deplacer(var curs : curseur; var grille :tab);
+procedure deplacer1(var curs : curseur; var grille :tab); // déplace le curseur avec les controles du joueur1
+procedure deplacer2(var curs : curseur;var grille : tab); // déplace le curseur avec les controles du joueur2
 
 
 
 Implementation
 
 
-procedure deplacer(var curs:curseur;var grille:tab);
+procedure deplacer1(var curs:curseur;var grille:tab);
 var touche:char;
 begin
     touche := READKEY;
@@ -21,9 +22,23 @@ begin
         GAUCHE: if curs.x > 1 then curs.x := curs.x - 1;  // déplacement gauche
         DROITE: if curs.x < MAX then curs.x := curs.x + 1;  // déplacement droite
         ENTREE: if not(grille[curs.y][curs.x].estAffiche) then grille[curs.y][curs.x].estAffiche:=True; // rends la case sur laquelle est le curseur dévoilable
-        INSER :  if not(grille[curs.y][curs.x].drapeau)   then grille[curs.y][curs.x].drapeau:=True;  // pose un drapeau ....
+        INSER :  if not(grille[curs.y][curs.x].drapeau)   then grille[curs.y][curs.x].drapeau:=True;  // pose un drapeau sur la case du curseur
     end;
 end;
 
+
+procedure deplacer2(var curs:curseur;var grille:tab);
+var touche:char;
+begin
+    touche := READKEY;
+    case touche of
+        z: if curs.y > 1 then curs.y := curs.y - 1;  // déplacement haut
+        S: if curs.y < MAX then curs.y := curs.y + 1;  // déplacement bas
+        Q: if curs.x > 1 then curs.x := curs.x - 1;  // déplacement gauche
+        D: if curs.x < MAX then curs.x := curs.x + 1;  // déplacement droite
+        A: if not(grille[curs.y][curs.x].estAffiche) then grille[curs.y][curs.x].estAffiche:=True; // rends la case sur laquelle est le curseur dévoilable
+        E:  if not(grille[curs.y][curs.x].drapeau)   then grille[curs.y][curs.x].drapeau:=True;  // pose un drapeau sur la case du curseur
+    end;
+end;
 
 end.
