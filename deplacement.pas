eedd5d22d60e5112ee12e88dfecb33e6e40fee81
Preuxchevalier <titouan.glemarec@insa-rouen.fr>
2024-11-06 10:21:56 +0100

    Update deplacement.pas
    Une seule fonction déplacer pour avoir un seul readkey.

--- deplacement.pas
+++ deplacement.pas
@@ -4,40 +4,33 @@ Interface
 
 uses crt, types_demineur;
 
-procedure deplacer1(var curs : curseur; var grille :tab); // déplace le curseur avec les controles du joueur1
-procedure deplacer2(var curs : curseur;var grille : tab); // déplace le curseur avec les controles du joueur2
+procedure deplacer(var curs1,curs2 : curseur; var grille1,grille2 :tab); // déplace les curseurs dans leur grille respective
+
 
 
 
 Implementation
 
 
-procedure deplacer1(var curs:curseur;var grille:tab);
+procedure deplacer(var curs1,curs2:curseur;var grille1,grille2:tab);
 var touche:char;
 begin
     touche := READKEY;
     case touche of
-        HAUT:  if curs.y < MAX then curs.y := curs.y + 1;// déplacement haut
-        BAS: if curs.y > 1 then curs.y := curs.y - 1;  // déplacement bas
-        GAUCHE: if curs.x > 1 then curs.x := curs.x - 1;  // déplacement gauche
-        DROITE: if curs.x < MAX then curs.x := curs.x + 1;  // déplacement droite
-        ENTREE: if not(grille[curs.y][curs.x].estAffiche) then grille[curs.y][curs.x].estAffiche:=True; // rends la case sur laquelle est le curseur dévoilable
-        INSER :  if not(grille[curs.y][curs.x].drapeau)   then grille[curs.y][curs.x].drapeau:=True;  // pose un drapeau sur la case du curseur
-    end;
-end;
-
-
-procedure deplacer2(var curs:curseur;var grille:tab);
-var touche:char;
-begin
-    touche := READKEY;
-    case touche of
-        z: if curs.y < MAX then curs.y := curs.y + 1;  // déplacement haut
-        S: if curs.y > 1 then curs.y := curs.y - 1;  // déplacement bas
-        Q: if curs.x > 1 then curs.x := curs.x - 1;  // déplacement gauche
-        D: if curs.x < MAX then curs.x := curs.x + 1;  // déplacement droite
-        A: if not(grille[curs.y][curs.x].estAffiche) then grille[curs.y][curs.x].estAffiche:=True; // rends la case sur laquelle est le curseur dévoilable
-        E:  if not(grille[curs.y][curs.x].drapeau)   then grille[curs.y][curs.x].drapeau:=True;  // pose un drapeau sur la case du curseur
+		//premier curseur dans la première grille :
+        HAUT:  if curs1.y < MAX then curs1.y := curs1.y + 1;// déplacement haut
+        BAS: if curs1.y > 1 then curs1.y := curs1.y - 1;  // déplacement bas
+        GAUCHE: if curs1.x > 1 then curs1.x := curs1.x - 1;  // déplacement gauche
+        DROITE: if curs1.x < MAX then curs1.x := curs1.x + 1;  // déplacement droite
+        ENTREE: if not(grille1[curs1.y][curs1.x].estAffiche) then grille1[curs1.y][curs1.x].estAffiche:=True; // rends la case sur laquelle est le curseur dévoilable
+        INSER :  if not(grille1[curs1.y][curs1.x].drapeau)   then grille1[curs1.y][curs1.x].drapeau:=True;  // pose un drapeau sur la case du curseur
+		//deuxième curseur dans la deuxième ligne:
+        z: if curs2.y < MAX then curs2.y := curs2.y + 1;  // déplacement haut
+        S: if curs2.y > 1 then curs2.y := curs2.y - 1;  // déplacement bas
+        Q: if curs2.x > 1 then curs2.x := curs2.x - 1;  // déplacement gauche
+        D: if curs2.x < MAX then curs2.x := curs2.x + 1;  // déplacement droite
+        A: if not(grille2[curs2.y][curs2.x].estAffiche) then grille2[curs2.y][curs2.x].estAffiche:=True; // rends la case sur laquelle est le curseur dévoilable
+        E:  if not(grille2[curs2.y][curs2.x].drapeau)   then grille2[curs2.y][curs2.x].drapeau:=True;  // pose un drapeau sur la case du curseur
     end;
 end;
 
