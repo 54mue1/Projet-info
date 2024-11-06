39e4b8fc53b933e2dba1ca71734cc7679cba1d71
Preuxchevalier <titouan.glemarec@insa-rouen.fr>
2024-11-06 11:01:31 +0100

    Update finPartie.pas

--- finPartie.pas
+++ finPartie.pas
@@ -10,12 +10,19 @@ Implementation
 
 
 function finDePartie(grille : tab): BOOLEAN;
-var i,j:integer;
+var i,j,k:integer;
 begin
+k:=0;
 for i := 1 to MAX do      
 	for j := 1 to MAX do
 		if grille[i][j].estAffiche and grille[i][j].mine then
 			finDePartie:=TRUE;
+for i := 1 to MAX do      
+	for j := 1 to MAX do
+		if grille[i][j].estAffiche  then
+			k:=k+1;
+If k=MAX*MAX-NB_BOMBES then
+	finDePartie:=TRUE
 end;
 
 end.
