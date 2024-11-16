afa7e62681c9bc443c97205d23bb140430bb9885
Preuxchevalier <titouan.glemarec@insa-rouen.fr>
2024-11-16 16:57:38 +0100

    Update deplacement.pas

--- deplacement.pas
+++ deplacement.pas
@@ -18,12 +18,12 @@ begin
     touche := READKEY;
     case touche of
 		//premier curseur dans la première grille :
-        HAUT:  if curs1.y < MAX then curs1.y := curs1.y + 1;// déplacement haut
-        BAS: if curs1.y > 1 then curs1.y := curs1.y - 1;  // déplacement bas
-        GAUCHE: if curs1.x > 1 then curs1.x := curs1.x - 1;  // déplacement gauche
-        DROITE: if curs1.x < MAX then curs1.x := curs1.x + 1;  // déplacement droite
-        ENTREE: if not(grille1[curs1.y][curs1.x].estAffiche) then grille1[curs1.y][curs1.x].estAffiche:=True; // rends la case sur laquelle est le curseur dévoilable
-        INSER :  if not(grille1[curs1.y][curs1.x].drapeau)   then grille1[curs1.y][curs1.x].drapeau:=True;  // pose un drapeau sur la case du curseur
+        O:  if curs1.y < MAX then curs1.y := curs1.y + 1;// déplacement haut
+        L: if curs1.y > 1 then curs1.y := curs1.y - 1;  // déplacement bas
+        K: if curs1.x > 1 then curs1.x := curs1.x - 1;  // déplacement gauche
+        M: if curs1.x < MAX then curs1.x := curs1.x + 1;  // déplacement droite
+        I: if not(grille1[curs1.y][curs1.x].estAffiche) then grille1[curs1.y][curs1.x].estAffiche:=True; // rends la case sur laquelle est le curseur dévoilable
+        P :  if not(grille1[curs1.y][curs1.x].drapeau)   then grille1[curs1.y][curs1.x].drapeau:=True;  // pose un drapeau sur la case du curseur
 		//deuxième curseur dans la deuxième ligne:
         z: if curs2.y < MAX then curs2.y := curs2.y + 1;  // déplacement haut
         S: if curs2.y > 1 then curs2.y := curs2.y - 1;  // déplacement bas
