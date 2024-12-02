b9c304e80682e543137dc32036be5bf20959fd8a
Preuxchevalier <titouan.glemarec@insa-rouen.fr>
2024-12-02 19:17:44 +0100

    Update deplacement.pas
    Possibilité d'enlever le drapeau s'il y en a un

--- deplacement.pas
+++ deplacement.pas
@@ -24,14 +24,14 @@ begin
         Q: if curs1.x > 1 then curs1.x := curs1.x - 1;  // déplacement gauche
         D: if curs1.x < MAX then curs1.x := curs1.x + 1;  // déplacement droite
         A: if not(grille1[curs1.y][curs1.x].estAffiche) then grille1[curs1.y][curs1.x].estAffiche:=True; // rends la case sur laquelle est le curseur dévoilable
-        E:  if not(grille2[curs1.y][curs1.x].drapeau)   then grille1[curs1.y][curs1.x].drapeau:=True;  // pose un drapeau sur la case du curseur
+        E:  if (grille2[curs1.y][curs1.x].drapeau)   then grille1[curs1.y][curs1.x].drapeau:=False else grille1[curs1.y][curs1.x].drapeau:=True ;  // enlève un drapeau s'il y en a déjà, sinon en ajoute un
 		//premier curseur dans la première grille :
         O:  if curs2.y < MAX then curs2.y := curs2.y + 1;// déplacement haut
         L: if curs2.y > 1 then curs2.y := curs2.y - 1;  // déplacement bas
         K: if curs2.x > 1 then curs2.x := curs2.x - 1;  // déplacement gauche
         M: if curs2.x < MAX then curs2.x := curs2.x + 1;  // déplacement droite
         I: if not(grille2[curs2.y][curs2.x].estAffiche) then grille2[curs2.y][curs2.x].estAffiche:=True; // rends la case sur laquelle est le curseur dévoilable
-        P :  if not(grille1[curs2.y][curs2.x].drapeau)   then grille2[curs2.y][curs2.x].drapeau:=True;  // pose un drapeau sur la case du curseur 
+        P :  if (grille1[curs2.y][curs2.x].drapeau)   then grille2[curs2.y][curs2.x].drapeau:=False else grille2[curs2.y][curs2.x].drapeau:=True;  // pose un drapeau sur la case du curseur 
 end;
 end;
 end.
