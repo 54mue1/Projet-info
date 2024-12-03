c8c2628480cdb0324b24574c5b59307ecf20ee8d
Preuxchevalier <titouan.glemarec@insa-rouen.fr>
2024-12-03 13:11:49 +0100

    Create gestionscores.pas

---
+++ gestionscores.pas
@@ -0,0 +1,72 @@
+unit gestionscores;
+
+Interface 
+
+uses SysUtils;
+
+
+const
+  FICHIER_SCORES = 'scores.txt';
+
+type
+  TScore = record
+    Temps: Integer; // Temps en secondes
+  end;
+
+procedure enregistrerScore(nouveauTemps: Integer);
+
+Implementation
+
+procedure enregistrerScore(nouveauTemps: Integer);
+var
+  fichier: TextFile;
+  ligne: string;
+  meilleurTemps, tempsActuel: LongInt;
+  estMeilleur: Boolean;
+begin
+  meilleurTemps := MaxInt; // Initialiser avec la valeur maximale possible
+  estMeilleur := False;
+
+  // On vérifie les scores précédents et on trouve le meilleur.
+  if FileExists(FICHIER_SCORES) then
+  begin
+    Assign(fichier, FICHIER_SCORES);
+    Reset(fichier);
+    while not EOF(fichier) do
+    begin
+      ReadLn(fichier, ligne);
+      if TryStrToInt(ligne, tempsActuel) then // On convertit la chaine du fichier texte en nombre si possible
+      begin
+        if tempsActuel < meilleurTemps then
+          meilleurTemps := tempsActuel;
+      end;
+    end;
+    Close(fichier);
+  end;
+  
+  // On ajoute le nouveau temps dans le fichier
+  assign(fichier, FICHIER_SCORES);
+  if FileExists(FICHIER_SCORES) then
+    Append(fichier)
+  else
+    Rewrite(fichier);
+
+  WriteLn(fichier, nouveauTemps);
+  Close(fichier);
+  
+  // On regarde si le meilleur temps est le meilleur
+  if nouveauTemps < meilleurTemps then
+  begin
+    meilleurTemps := nouveauTemps;
+    estMeilleur := True;
+  end;
+
+
+
+  // Affichage du meilleur temps avec annonce si record battu ou non 
+  if estMeilleur then
+    WriteLn('Nouveau record battu ! Meilleur temps : ', meilleurTemps, ' secondes.')
+  else
+    WriteLn('Temps enregistré. Meilleur temps actuel : ', meilleurTemps, ' secondes.');
+end;
+
