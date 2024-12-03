c0445311932d41d23932ac89f3207ff6cd445eab
54mue1 <107001860+54mue1@users.noreply.github.com>
2024-12-03 15:21:46 +0100

    Update chrono.pas

--- chrono.pas
+++ chrono.pas
@@ -9,35 +9,29 @@ procedure TimeDiff(HeureDeb,MinuteDeb,SecondeDeb,CentiemeDeb,HeureFin,MinuteFin,
 implementation
 
 procedure TimeDiff(HeureDeb,MinuteDeb,SecondeDeb,CentiemeDeb,HeureFin,MinuteFin,SecondeFin,CentiemeFin:Word;var tempsEnSecondes:Word);
-var heure,minute,seconde:Word;
+var minute,seconde:Word;
 begin
-	heure:=0;
 	minute:=0;
 	seconde:=0;
-	if (HeureFin-HeureDeb)<>0 then
-		begin
-			if MinuteFin>MinuteDeb then 
-				heure:=HeureFin-HeureDeb	
-			else 
-				heure:=HeureFin-HeureDeb-1;
-		end;
 	if (MinuteFin-MinuteDeb)<>0 then
 		begin
 			if SecondeFin>SecondeDeb then 
-				minute:=MinuteFin-MinuteDeb
-			else 
-				minute:=60-MinuteDeb+MinuteFin;
-		end;
-	if (SecondeFin-SecondeDeb)<>0 then
-		begin
-			if CentiemeFin>CentiemeDeb then 
-				seconde:=SecondeFin-SecondeDeb
-			else 
-				seconde:=60-SecondeFin+SecondeDeb;
-		end;
-writeln('Chrono de fin de partie',heure,' : ',minute,' : ',seconde);
-tempsEnSecondes:=heure*3600+minute*60+seconde;
+				begin
+					minute:=MinuteFin-MinuteDeb;	
+					seconde:=SecondeFin-SecondeDeb;
+				end
+			else
+				begin
+					minute:=MinuteFin-MinuteDeb-1;
+					seconde:=60-SecondeDeb+SecondeFin;
+				end;
+		end
+	else
+		seconde:=SecondeFin-SecondeDeb;		
+writeln('La partie à durée :',minute,' minutes et ', seconde ,' secondes');
+tempsEnSecondes:=minute*60+seconde;
 end;
 end.
+
 
 
