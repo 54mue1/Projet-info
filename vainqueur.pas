620294d8335ca45f79a3409ddbf95f6203c10e49
pauloow14 <paul.gauclin@laposte.net>
2024-12-04 10:25:43 +0100

    Update vainqueur.pas

--- vainqueur.pas
+++ vainqueur.pas
@@ -9,6 +9,60 @@ procedure victoire(estTermine1, estTermine2, estGagne1, estGagne2: Boolean);
 Implementation	
 
 procedure victoire(estTermine1, estTermine2, estGagne1, estGagne2: Boolean);
+var i: Integer;
+begin
+if estTermine1 then 
+	begin
+		gotoxy(XGRILLE1,YGRILLE-4);
+ 		TextColor(12);
+		write('Perdu');
+		TextColor(15);
+		gotoxy(XGRILLE2,YGRILLE-4);
+		write('Gagné');
+	end;
+if estTermine2 then 
+	begin
+		gotoxy(XGRILLE1,YGRILLE-4);
+		TextColor(15);
+		write('Gagné');
+		TextColor(12);
+		gotoxy(XGRILLE2,YGRILLE-4);
+		write('Perdu');
+	end;
+if estGagne1 then 
+	begin
+		gotoxy(XGRILLE1,YGRILLE-4);
+		TextColor(15);
+		write('Gagné');
+		TextColor(12);
+		gotoxy(XGRILLE2,YGRILLE-4);
+		write('Perdu');
+	end;
+if estGagne2 then 
+	begin
+		gotoxy(XGRILLE1,YGRILLE-4);
+		TextColor(12);
+		write('Perdu');
+		TextColor(15);
+		gotoxy(XGRILLE2,YGRILLE-4);
+		write('Gagné');
+	end;
+for i:=1 to 13 do
+	writeln();
+end;
+
+end.
+unit vainqueur;
+
+Interface
+
+uses types_demineur, crt;
+
+procedure victoire(estTermine1, estTermine2, estGagne1, estGagne2: Boolean);
+
+Implementation	
+
+procedure victoire(estTermine1, estTermine2, estGagne1, estGagne2: Boolean);
 begin
 if estTermine1 then 
 	begin
