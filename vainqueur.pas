0792a150f695df56eb054abf6f1f7a87aee6a4ea
Preuxchevalier <titouan.glemarec@insa-rouen.fr>
2024-12-17 12:51:22 +0100

    Update vainqueur.pas

--- vainqueur.pas
+++ vainqueur.pas
@@ -11,24 +11,7 @@ Implementation
 procedure victoire(estPerdu1, estPerdu2, estGagne1, estGagne2: Boolean);  //Determine quel joueur à gagner en fonction de la manière dont se termine la partie
 var i: Integer;
 begin
-{if estPerdu1 then // si joueur de la grille de gauche est tombé sur une bombe
-	begin
-		TextColor(12);
-		gotoxy(XGRILLE1,YGRILLE-4);
-		write('Perdu');
-		TextColor(15);
-		gotoxy(XGRILLE2,YGRILLE-4); 
-		write('Gagné');
-	end;
-if estPerdu2 then // si joueur de la grille de droite est tombé sur une bombe
-	begin
-		gotoxy(XGRILLE1,YGRILLE-4);
-		TextColor(15);
-		write('Gagné');
-		TextColor(12);
-		gotoxy(XGRILLE2,YGRILLE-4);
-		write('Perdu');
-	end;}
+
 if estGagne1 or estPerdu2 then // si joueur de la grille de gauche a trouvé toutes les bombes
 	begin
 		gotoxy(XGRILLE1,YGRILLE-4);
